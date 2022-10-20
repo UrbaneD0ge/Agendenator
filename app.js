2e7d7b3845a9116a72fc66c2bdf2aa540ce0d414
Kip Dunlap <kipling.dunlap@gmail.com>
2022-10-20 17:35:09 -0400

    assemble the doc

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let NPU = document.getElementById('NPU').value;
+let NPU = document.getElementById('NPU')
+let date = document.getElementById('date')
+
+let mtgDate = new Date(date.value).toDateString();
+console.log(mtgDate);
 
 // on change, console log the value of NPU
-NPU.addEventListener('change', function () {
+NPU.addEventListener('change', NPUid);
+date.addEventListener('change', dateid);
+
+function NPUid() {
   console.log(NPU.value);
-});
+  document.getElementById('NPUletter').innerText = NPU.value;
+};
+
+function dateid() {
+  console.log(mtgDate);
+  document.getElementById('datePlc').innerText = mtgDate;
+}
