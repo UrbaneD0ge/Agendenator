760f61c1a6189a1ce397eba1f13f8fe0c6424b55
Kip Dunlap <kipling.dunlap@gmail.com>
2022-10-25 16:28:21 -0400

    basic setup

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-let NPU = document.getElementById('NPU')
-let date = document.getElementById('date')
-
-let mtgDate = new Date(date.value).toDateString();
-console.log(mtgDate);
+let NPU = document.getElementById('NPU');
+let date = document.getElementById('date');
 
 // on change, console log the value of NPU
 NPU.addEventListener('change', NPUid);
-date.addEventListener('change', dateid);
+// date.addEventListener('change', dateid);
 
 function NPUid() {
   console.log(NPU.value);
   document.getElementById('NPUletter').innerText = NPU.value;
 };
 
+// on ready, set the datepicker to today's date
+window.onload = function () {
+  var today = new Date();
+  document.getElementById('date').value = today.toISOString().slice(0, 10);
+  document.getElementById('datePlc').innerText = today.toDateString();
+};
+
+// on change, replace the date in the letter
+date.addEventListener('change', dateid);
+
 function dateid() {
-  console.log(mtgDate);
-  document.getElementById('datePlc').innerText = mtgDate;
+  document.getElementById('datePlc').innerText = date.value;
 }
