ea2e2022876150d748f459e66abd236504cf065b
Kip Dunlap <kipling.dunlap@gmail.com>
2022-11-15 16:06:44 -0500

    NEW route works again, Save does not.

--- routes/applications.js
+++ routes/applications.js
@@ -7,38 +7,36 @@ router.get('/new', (req, res) => {
   res.render('applications/new', { applications: new Application() });
 });
 
+router.get('/edit/:id', async (req, res) => {
+  const application = await Application.findById(req.params.id);
+  res.render('applications/edit', { application: application });
+});
+
 router.get('/:slug', async (req, res) => {
   const application = await Application.findOne({ slug: req.params.slug });
   if (application == null) res.redirect('/')
   res.render('applications/show', { application: application });
 });
 
-router.get('/edit/:id', async (req, res) => {
-  const application = await Application.findById(req.params.id);
-  if (application == null) res.redirect('/')
-  res.render('applications/edit', { application: application });
-});
-
-router.put('edit/:id', async (req, res, next) => {
-  req.application = await Application.findById(req.params.id)
-  if (application == null) res.redirect('/applications/show')
-  next()
-}, saveAndRedirect('/applications/show'));
-
-router.get('/', async (req, res) => {
-  const applications = await Application.find().sort({ NPU: 'asc' });
-  await res.render('applications/applications', { applications: applications });
-});
-
 router.post('/', async (req, res, next) => {
   req.application = new Application()
   next()
-}, saveAndRedirect('/show'));
+}, saveAndRedirect('show'));
+
+router.put('/:id', async (req, res, next) => {
+  req.application = await Application.findById(req.params.id)
+  next()
+}, saveAndRedirect('show'));
 
 router.delete('/:id', async (req, res) => {
   await Application.findByIdAndDelete(req.params.id)
   res.redirect('/applications')
 })
+
+router.get('/', async (req, res) => {
+  const applications = await Application.find().sort({ NPU: 'asc' });
+  await res.render('applications/applications', { applications: applications });
+});
 
 function saveAndRedirect(path) {
   return async (req, res) => {
@@ -53,10 +51,10 @@ function saveAndRedirect(path) {
     application.notes = req.body.notes
     try {
       application = await application.save()
-      res.redirect(`/ applications / ${application.id}`)
+      res.redirect(`/show/${application.slug}`)
     } catch (err) {
       console.log(err)
-      res.render(`applications / ${path}`, { application: application })
+      res.render(`applications/${path}`, { application: application })
     }
   }
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const applicationRouter = require('./routes/applications');
 const Application = require('./models/applications');
+const NPU = require('./models/NPUs');
 const methodOverride = require('method-override');
 
 app.listen(3000 || process.env.PORT, () => {
@@ -28,17 +29,6 @@ mongoose.connect("mongodb+srv://UrbaneDoge:bPlZ8wc1DQ4cnhQC@cluster0.lojy1rw.mon
   console.log('Could not connect to the database. Exiting now...', err);
   process.exit();
 });
-
-// run()
-async function run() {
-  Application.find({ NPU: 'Q' }, function (err, doc) {
-    if (err) return handleError(err);
-    Object.keys(doc).forEach(function (key) {
-      var val = doc[key];
-      console.log(val);
-    });
-  });
-}
 
 app.use('/applications', applicationRouter);
 app.use('/show', applicationRouter);
